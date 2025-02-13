af81a1ee94d235d7ff44f7401ec8e7d9d9ac78c4
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2025-02-13 15:10:11 -0300

    initial commit

---
+++ src/test/java/br/com/helio/springauthserver/SpringAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.helio.springauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
